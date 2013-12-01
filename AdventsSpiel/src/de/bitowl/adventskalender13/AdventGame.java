9d1d1ab8eb5e946bae49d6c441f55c74081bf71e
bitowl <bitowl@googlemail.com>
2013-12-01 22:51:28 +0100

    Tag 3 bearbeitet

--- AdventsSpiel/src/de/bitowl/adventskalender13/AdventGame.java
+++ AdventsSpiel/src/de/bitowl/adventskalender13/AdventGame.java
@@ -2,6 +2,8 @@ package de.bitowl.adventskalender13;
 
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
@@ -9,6 +11,7 @@ import com.badlogic.gdx.graphics.Texture.TextureFilter;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector3;
 
 public class AdventGame implements ApplicationListener {
 
@@ -20,6 +23,9 @@ public class AdventGame implements ApplicationListener {
 	TextureRegion christmasBall;
 	
 	OrthographicCamera camera;
+	
+	float ballX; // Position des Balles 
+	float ballY;
 
 	@Override
 	public void create() {
@@ -34,6 +40,8 @@ public class AdventGame implements ApplicationListener {
 		ball = new Texture(Gdx.files.internal("graphics/ball.png"));
 		christmasBall = new TextureRegion(ball, 0,0,90,80);
 		
+		
+		Gdx.input.setInputProcessor(new MyInputProcessor());
 	}
 
 	@Override
@@ -49,7 +57,15 @@ public class AdventGame implements ApplicationListener {
 		batch.setProjectionMatrix(camera.combined); // mal wo kamara hingugt!
 		batch.begin();
 		batch.draw(christmasTree,0,0);
-		batch.draw(christmasBall,0,0);
+	/*	if(Gdx.input.isTouched()){
+
+			Vector3 touchPos = new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0);
+			camera.unproject(touchPos);
+			Gdx.app.log("mouse pressed",touchPos.x + ", "+touchPos.y);
+
+			//ballY += Gdx.graphics.getDeltaTime()*300;
+		}*/
+		batch.draw(christmasBall,ballX,ballY);
 		batch.end();
 	}
 
@@ -64,5 +80,63 @@ public class AdventGame implements ApplicationListener {
 
 	@Override
 	public void resume() {
+	}
+	
+	class MyInputProcessor implements InputProcessor{
+		Vector3 touchPos = new Vector3();
+		
+		@Override
+		public boolean keyDown(int keycode) {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+		@Override
+		public boolean keyUp(int keycode) {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+		@Override
+		public boolean keyTyped(char character) {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+		@Override
+		public boolean touchDown(int screenX, int screenY, int pointer,
+				int button) {
+			return false;
+		}
+
+		@Override
+		public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+		@Override
+		public boolean touchDragged(int screenX, int screenY, int pointer) {
+			
+			touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
+			camera.unproject(touchPos);
+			ballX = touchPos.x - christmasBall.getRegionWidth()/2;
+			ballY = touchPos.y - christmasBall.getRegionHeight()/2;
+			
+			return false;
+		}
+
+		@Override
+		public boolean mouseMoved(int screenX, int screenY) {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+		@Override
+		public boolean scrolled(int amount) {
+			// TODO Auto-generated method stub
+			return false;
+		}
+		
 	}
 }
