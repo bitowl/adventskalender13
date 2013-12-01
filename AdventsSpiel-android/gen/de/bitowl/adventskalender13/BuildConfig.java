25220037faab831bd59bd6cb6eefeedc5ea56963
bitowl <bitowl@googlemail.com>
2013-12-01 22:37:01 +0100

    Tag 2 bearbeitet

---
+++ AdventsSpiel-android/gen/de/bitowl/adventskalender13/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package de.bitowl.adventskalender13;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
--- AdventsSpiel/src/de/bitowl/adventskalender13/AdventGame.java
+++ AdventsSpiel/src/de/bitowl/adventskalender13/AdventGame.java
@@ -11,24 +11,51 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class AdventGame implements ApplicationListener {
+
+	SpriteBatch batch;
 	
+	Texture tree;
+	TextureRegion christmasTree;
+	Texture ball;
+	TextureRegion christmasBall;
+	
+	OrthographicCamera camera;
+
 	@Override
-	public void create() {		
-	
+	public void create() {
+		batch = new SpriteBatch();
+		
+		camera=new OrthographicCamera();
+		camera.setToOrtho(false, 800,480);
+		
+		tree = new Texture(Gdx.files.internal("graphics/tree.png"));
+		christmasTree=new TextureRegion(tree, 0, 0, 450,730);
+		
+		ball = new Texture(Gdx.files.internal("graphics/ball.png"));
+		christmasBall = new TextureRegion(ball, 0,0,90,80);
+		
 	}
 
 	@Override
 	public void dispose() {
+		tree.dispose();
 	}
 
 	@Override
-	public void render() {		
-		Gdx.gl.glClearColor((float) Math.random(),(float) Math.random(), (float) Math.random(), 1);
+	public void render() {
+		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+
+		batch.setProjectionMatrix(camera.combined); // mal wo kamara hingugt!
+		batch.begin();
+		batch.draw(christmasTree,0,0);
+		batch.draw(christmasBall,0,0);
+		batch.end();
 	}
 
 	@Override
 	public void resize(int width, int height) {
+		// camera.setToOrtho(false,width,height);
 	}
 
 	@Override
